5b824d4ebc1a3adcab8a5ce03e5652d9a612aad1
studentaccount2028 <tpark2028@kehillahstudent.org>
2025-01-15 20:50:11 -0800

    finished

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ password = "hi potato zzz"
 # make my password secret
 key = 4
 
-def encrypt(plaintext):
+def encrypt(plaintext, key):
     ciphertext = ""
     for letter in plaintext:
         old_position = alphabet.find(letter)
@@ -22,8 +22,36 @@ def encrypt(plaintext):
             ciphertext += new_letter
     return ciphertext
 
-print(encrypt(password))
+# print(encrypt(password, key))
 
 # Your task:
 # figure out what key I used to encrypt this message:
 secret_message = "y qc q iushuj cuiiqwu oek mybb duluh wkuii"
+
+
+def decrypt(secret_message, key):
+    text = ""
+    for letter in secret_message:
+        old_position = alphabet.find(letter)
+        if old_position == -1:
+            text += " "
+        else:
+            new_position = old_position - key
+            new_position = new_position % len(alphabet)
+            new_letter = alphabet[new_position]
+            text += new_letter
+    return text
+    # old_position = new_position - key
+    # old_position = old_position * len(alphabet)
+    # old_letter = alphabet[old_position]
+    # decryptedtest = decryptedtext - old_letter
+
+print(decrypt("y qc q iushuj cuiiqwu oek mybb duluh wkuii", key))
+
+for i in range(50):
+    print(decrypt("y qc q iushuj cuiiqwu oek mybb duluh wkuii", 16))
+
+# # key = 1
+# # for i in range(100000):
+# #     cipherencrypt(password(key))
+# #     if 
